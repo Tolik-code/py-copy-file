f9868b3d5a426ea3368d3d95f5f4311a9fa29323
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-03-27 16:43:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import shutil
+
+
+def copy_file(command: str) -> None:
+    try:
+        source_file_name, target_file_name = command[3:].split(" ")
+
+        try:
+            if source_file_name != target_file_name:
+                shutil.copyfile(source_file_name, target_file_name)
+        except FileNotFoundError:
+            print(f"Not found file with name: {source_file_name}")
+
+    except ValueError:
+        print(f"Cannot parse file names from command: {command}")
