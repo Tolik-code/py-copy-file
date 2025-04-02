b9c9681e4dc4c5ce79d829149ac1eba5fb1ac3de
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-02 13:46:05 +0300

    Added additional check for num of parts

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ import shutil
 
 def copy_file(command: str) -> None:
     try:
+        if len(command.split(" ")) != 3: #additional check, for tests AI buddy
+            raise ValueError
+
         copy_command, source_file_name, target_file_name = command.split(" ")
 
         if copy_command != "cp":
